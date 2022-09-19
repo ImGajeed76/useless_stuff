832422ed6581c8b1ba8c9f8e747e08cdfe6b5e79
76img <76img>
2022-09-19 11:57:17 +0200

    added some stuff

--- useless/useless_converter.py
+++ useless/useless_converter.py
@@ -31,3 +31,72 @@ def degree_to_radian(degree):
 
 def radian_to_degree(radian):
     return radian * 180 / math.pi
+
+
+def rgb_to_hvs(rgb):
+    r, g, b = rgb
+    h = 0
+
+    c_max = max(r, g, b)
+    c_min = min(r, g, b)
+    delta = c_max - c_min
+
+    if delta == 0:
+        h = 0
+    elif c_max == r:
+        h = 60 * (((g - b) / delta) % 6)
+    elif c_max == g:
+        h = 60 * (((b - r) / delta) + 2)
+    elif c_max == b:
+        h = 60 * (((r - g) / delta) + 4)
+
+    if c_max == 0:
+        s = 0
+    else:
+        s = delta / c_max
+
+    v = c_max
+
+    return h, s, v
+
+
+def hsv_to_rgb(hsv):
+    h, s, v = hsv
+    r = 0
+    g = 0
+    b = 0
+
+    c = v * s
+    x = c * (1 - abs((h / 60) % 2 - 1))
+    m = v - c
+
+    if 0 <= h < 60:
+        r = c
+        g = x
+        b = 0
+    elif 60 <= h < 120:
+        r = x
+        g = c
+        b = 0
+    elif 120 <= h < 180:
+        r = 0
+        g = c
+        b = x
+    elif 180 <= h < 240:
+        r = 0
+        g = x
+        b = c
+    elif 240 <= h < 300:
+        r = x
+        g = 0
+        b = c
+    elif 300 <= h < 360:
+        r = c
+        g = 0
+        b = x
+
+    r = (r + m) * 255
+    g = (g + m) * 255
+    b = (b + m) * 255
+
+    return r, g, b
